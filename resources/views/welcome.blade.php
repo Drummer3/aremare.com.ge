08b88dbcb257612dafef68c91b14ef989061a7b6
Drummer3 <drummerthree@gmail.com>
2022-01-29 01:43:49 +0400

    responsive padding on white thingy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body class="bg-neutral-800 antialiased flex justify-center items-center h-screen">
-        <div class="bg-neutral-200/90 text-orange-600 p-16 rounded-3xl shadow-md shadow-orange-600/50">
+        <div class="bg-neutral-200/90 text-orange-700 py-16 px-20  md:py-20 md:px-24 xl:py-28 xl:px-32 rounded-3xl shadow-md shadow-orange-700/50">
             <p class="text-4xl font-mono animate-bounce drop-shadow-xl">Coming Soon</p>
         </div>
     </body>
